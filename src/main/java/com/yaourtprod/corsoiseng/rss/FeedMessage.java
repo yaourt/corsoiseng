a80978fecac4f546420727ea0f8431081465382f
Yaourt <yaourt@yaourtprod.net>
2013-04-10 14:22:16 +0200

    WIP

---
+++ src/main/java/com/yaourtprod/corsoiseng/rss/FeedMessage.java
@@ -0,0 +1,66 @@
+package com.yaourtprod.corsoiseng.rss;
+
+public class FeedMessage {
+
+	  String title;
+	  String description;
+	  String link;
+	  String author;
+	  String guid;
+
+	  public String getTitle() {
+	    return title;
+	  }
+
+	  public void setTitle(String title) {
+	    this.title = title;
+	  }
+
+	  public String getDescription() {
+	    return description;
+	  }
+
+	  public void setDescription(String description) {
+	    this.description = description;
+	  }
+
+	  public String getLink() {
+	    return link;
+	  }
+
+	  public void setLink(String link) {
+	    this.link = link;
+	  }
+
+	  public String getAuthor() {
+	    return author;
+	  }
+
+	  public void setAuthor(String author) {
+	    this.author = author;
+	  }
+
+	  public String getGuid() {
+	    return guid;
+	  }
+
+	  public void setGuid(String guid) {
+	    this.guid = guid;
+	  }
+
+	  @Override
+	  public String toString() {
+		  return new StringBuilder("FeedMessage [title=")
+		  	.append(title)
+		  	.append(", description=")
+		  	.append(description)
+		  	.append(", link=")
+		  	.append(link)
+		  	.append(", author=")
+		  	.append(author)
+		  	.append(", guid=")
+		  	.append("]")
+		  	.toString();
+	  }
+
+}
